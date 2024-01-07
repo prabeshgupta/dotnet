e02fb2d34d6b5b94bcdf136ac624c016a5fc166b
Prabesh Gupta <prabeshtech232@gmail.com>
2024-01-07 19:19:31 +0545

    Learning C#

---
+++ FirstProgram.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace app
+{
+    internal class FirstProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Swapping.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace app
+{
+    class Swapping
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            num1 = num1 + num2;
+            num2 = num1 - num2;
+            num1 = num1 - num2;
+
+            // Console.WriteLine("Number 1: " + num1);
+            // Console.WriteLine("Number 2: " + num2);
+
+            Console.WriteLine("Number 1: {0}\nNumber 2: {1}", num1, num2);
+
+        }
+    }
+}
